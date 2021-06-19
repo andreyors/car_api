d1a0cc600a82f677e6a0a30e31014e5d38763e35
Andrey Orsoev <andrey.orsoev@gmail.com>
2021-06-19 15:01:14 +0200

    Fix case

--- tests/unit.rb
+++ tests/unit.rb
@@ -6,22 +6,22 @@ require File.dirname(File.dirname(__FILE__)) + '/models/init'
 
 class GeoPointTest < Test::Unit::TestCase
   def test_should_return_error_for_wrong_latitude
-    geoPoint = GeoPoint.new(100, 0)
-    assert_false geoPoint.valid?
+    geo_point = GeoPoint.new(100, 0)
+    assert_false geo_point.valid?
   end
 
   def test_should_return_error_for_wrong_longitude
-    geoPoint = GeoPoint.new(0, 200)
-    assert_false geoPoint.valid?
+    geo_point = GeoPoint.new(0, 200)
+    assert_false geo_point.valid?
   end
 
   def test_should_return_error_for_wrong_latitude_and_longitude
-    geoPoint = GeoPoint.new(100, 200)
-    assert_false geoPoint.valid?
+    geo_point = GeoPoint.new(100, 200)
+    assert_false geo_point.valid?
   end
 
   def test_should_return_ok
-    geoPoint = GeoPoint.new(0, 0)
-    assert geoPoint.valid?
+    geo_point = GeoPoint.new(0, 0)
+    assert geo_point.valid?
   end
 end
